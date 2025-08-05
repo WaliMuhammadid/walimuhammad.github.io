9bdb4270e7e694bfd4d48698cb4b718f01530159
wali muhammad <walimuhammadid@gmail.com>
2025-08-05 17:08:31 +0500

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "564170234"
+    serviceWorkerVersion: "3101467354"
   }
 });
